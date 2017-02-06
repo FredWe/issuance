f36b0c17f8de0a4f2b3107900963f5d5e5390554
Fred.W <fredw.mail@gmail.com>
2017-02-06 16:41:15 +0100

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var USERNAME = 'mateogianolio', // github username
-    REPO = 'openblog', // github repository
+var USERNAME = 'lifesinger', // github username
+    REPO = 'blog', // github repository
     TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
